efee1209cdffd1474ab3f2836c8a27f46ef32827
Jakub Olcha <olo75jg12@gmail.com>
2024-04-07 15:05:15 +0200

    react basics

--- react/55-first-r3f-application/src/Experience.js
+++ react/55-first-r3f-application/src/Experience.js
@@ -1,9 +1,28 @@
+import { useFrame } from '@react-three/fiber'
+import { useRef } from 'react';
+
 export default function Experience() {
+  const cubeRef = useRef();
+
+  useFrame((state, delta) => {
+    cubeRef.current.rotation.y += delta
+  })
+
   return (
     <>
-      <mesh>
-        <sphereGeometry args={[1.5, 32, 32]} />
-        <meshBasicMaterial />
+      <mesh position-x={-2}>
+        <sphereGeometry scale={1} />
+        <meshBasicMaterial color='orange' />
+      </mesh>
+
+      <mesh ref={cubeRef} rotation-y={Math.PI * 0.25} position-x={2} scale={1.5}>
+        <boxGeometry scale={1.5} />
+        <meshBasicMaterial color='mediumpurple' />
+      </mesh>
+
+      <mesh rotation-x={- Math.PI * 0.5} position-y={-1} scale={10}>
+        <planeGeometry scale={1} />
+        <meshBasicMaterial color='greenyellow' />
       </mesh>
     </>
   )
