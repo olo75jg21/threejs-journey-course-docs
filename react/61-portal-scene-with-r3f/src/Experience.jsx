a7007120be3436a82cda78b20497965e70b43d98
Jakub Olcha <olo75jg12@gmail.com>
2024-04-26 23:54:07 +0200

    before adding shaders

--- react/61-portal-scene-with-r3f/src/Experience.jsx
+++ react/61-portal-scene-with-r3f/src/Experience.jsx
@@ -1,17 +1,49 @@
-import { useGLTF, OrbitControls } from '@react-three/drei'
+import { Sparkles, useTexture, Center, useGLTF, OrbitControls } from '@react-three/drei'
 
 export default function Experience() {
     const { nodes } = useGLTF('./model/portal.glb');
-    console.log(model)
+
+    const bakedTexture = useTexture('./model/baked.jpg')
 
     return <>
         <color args={['#030202']} attach='background' />
 
         <OrbitControls makeDefault />
 
-        <mesh scale={1.5}>
-            <boxGeometry />
-            <meshNormalMaterial />
-        </mesh>
+        <Center>
+            <mesh geometry={nodes.baked.geometry}>
+                <meshBasicMaterial map={bakedTexture} map-flipY={false} />
+            </mesh>
+
+            <mesh
+                geometry={nodes.poleLightA.geometry}
+                position={nodes.poleLightA.position}
+            >
+                <meshBasicMaterial color='#ffffe5' />
+            </mesh>
+
+            <mesh
+                geometry={nodes.poleLightB.geometry}
+                position={nodes.poleLightB.position}
+            >
+                <meshBasicMaterial color='#ffffe5' />
+            </mesh>
+
+            <mesh
+                geometry={nodes.portalLight.geometry}
+                position={nodes.portalLight.position}
+                rotation={nodes.portalLight.rotation}
+            >
+                {/* <shaderMaterial /> */}
+            </mesh>
+
+            <Sparkles
+                size={6}
+                scale={[4, 2, 4]}
+                position-y={1}
+                speed={0.2}
+                count={40}
+            />
+        </Center>
     </>
 }
