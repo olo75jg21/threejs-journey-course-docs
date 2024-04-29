d98f4b39dce129dde09effe0eb271f97ddcae60e
Jakub Olcha <olo75jg12@gmail.com>
2024-04-29 23:17:25 +0200

    mouse events finis

--- react/62-mouse-events-with-r3f/src/Experience.jsx
+++ react/62-mouse-events-with-r3f/src/Experience.jsx
@@ -1,37 +1,59 @@
+import * as THREE from 'three'
 import { useFrame } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
+import { meshBounds, OrbitControls, useGLTF } from '@react-three/drei'
 import { useRef } from 'react'
 
-export default function Experience()
-{
+
+export default function Experience() {
     const cube = useRef()
-    
-    useFrame((state, delta) =>
-    {
+
+    useFrame((state, delta) => {
         cube.current.rotation.y += delta * 0.2
     })
 
-    return <>
+    const eventHandler = (event) => {
+        cube.current.material.color.set(`hsl(${Math.random() * 360}, 100%, 75%)`)
+    }
 
+    const texture = useGLTF('./hamburger.glb')
+
+    return <>
         <OrbitControls makeDefault />
 
-        <directionalLight position={ [ 1, 2, 3 ] } intensity={ 4.5 } />
-        <ambientLight intensity={ 1.5 } />
+        <directionalLight position={[1, 2, 3]} intensity={4.5} />
+        <ambientLight intensity={1.5} />
 
-        <mesh position-x={ - 2 }>
+        <mesh position-x={- 2} onClick={(event) => { event.stopPropagation() }}>
             <sphereGeometry />
             <meshStandardMaterial color="orange" />
         </mesh>
 
-        <mesh ref={ cube } position-x={ 2 } scale={ 1.5 }>
+        <mesh
+            ref={cube}
+            raycast={meshBounds}
+            position-x={2}
+            scale={1.5}
+            onClick={eventHandler}
+            onPointerEnter={() => { document.body.style.cursor = 'pointer' }}
+            onPointerLeave={() => { document.body.style.cursor = 'default' }}
+        >
             <boxGeometry />
             <meshStandardMaterial color="mediumpurple" />
         </mesh>
 
-        <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
+        <mesh position-y={- 1} rotation-x={- Math.PI * 0.5} scale={10}>
             <planeGeometry />
             <meshStandardMaterial color="greenyellow" />
         </mesh>
 
+        <primitive
+            object={texture.scene}
+            scale={0.25}
+            position-y={1}
+            onClick={(event) => {
+                event.stopPropagation()
+                console.log(event.object)
+            }}
+        />
     </>
 }
