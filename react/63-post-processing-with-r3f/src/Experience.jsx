73857604c69adb0b4eae4034dc29eda0c82f6b8d
Jakub Olcha <olo75jg12@gmail.com>
2024-05-13 18:53:57 +0200

    portfolio starter

--- react/63-post-processing-with-r3f/src/Experience.jsx
+++ react/63-post-processing-with-r3f/src/Experience.jsx
@@ -1,28 +1,36 @@
 import { OrbitControls } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
+import { EffectComposer, ToneMapping, Vignette } from '@react-three/postprocessing'
+import { BlendFunction } from 'postprocessing'
 
-export default function Experience()
-{
+export default function Experience() {
     return <>
+        <EffectComposer disableNormalPass>
+            <Vignette
+                offset={0.3}
+                darkness={0.9}
+            />
+            <ToneMapping />
+        </EffectComposer>
 
         <Perf position="top-left" />
 
         <OrbitControls makeDefault />
 
-        <directionalLight castShadow position={ [ 1, 2, 3 ] } intensity={ 4.5 } />
-        <ambientLight intensity={ 1.5 } />
+        <directionalLight castShadow position={[1, 2, 3]} intensity={4.5} />
+        <ambientLight intensity={1.5} />
 
-        <mesh castShadow position-x={ - 2 }>
+        <mesh castShadow position-x={- 2}>
             <sphereGeometry />
             <meshStandardMaterial color="orange" />
         </mesh>
 
-        <mesh castShadow position-x={ 2 } scale={ 1.5 }>
+        <mesh castShadow position-x={2} scale={1.5}>
             <boxGeometry />
             <meshStandardMaterial color="mediumpurple" />
         </mesh>
 
-        <mesh receiveShadow position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
+        <mesh receiveShadow position-y={- 1} rotation-x={- Math.PI * 0.5} scale={10}>
             <planeGeometry />
             <meshStandardMaterial color="greenyellow" />
         </mesh>
